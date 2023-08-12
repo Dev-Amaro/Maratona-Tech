9a758214e74ca005d629b160c8c696ede73b6691
Dev-Amaro <joao.guilherme1827@gmail.com>
2023-08-12 13:15:43 -0300

    enviar

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+document.getElementById("div-diagonal").addEventListener("click", function(){
+  document.querySelector("body").setAttribute("class","laranja");
+})
+
+document.getElementById("div-diagonal2").addEventListener("click", function(){
+  document.querySelector("body").setAttribute("class","vermelho");
+})
+
+document.getElementById("div-diagonal3").addEventListener("click", function(){
+  document.querySelector("body").setAttribute("class","laranja");
+})
+document.getElementById("div-diagonal4").addEventListener("click", function(){
+  document.querySelector("body").setAttribute("class","purpura");
+})
+document.getElementById("div-diagonal5").addEventListener("click", function(){
+  document.querySelector("body").setAttribute("class","vermelho");
+})
+document.getElementById("div-diagonal6").addEventListener("click", function(){
+  document.querySelector("body").setAttribute("class","laranja");
+})
